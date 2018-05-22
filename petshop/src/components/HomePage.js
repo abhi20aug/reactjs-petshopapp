4f0904984d3f71ca83618a2d69f47f690c0e9249
Abhishek Agrawal <abhi20aug@gmail.com>
2018-05-22 22:32:59 +0530

    inline style

--- petshop/src/components/HomePage.js
+++ petshop/src/components/HomePage.js
@@ -2,10 +2,21 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var HomePage = function(){
+
+    //inline styling
+    var style = {
+        textalign: 'center',
+        fontSize: '3em',
+        color: 'blue'
+    };
+    
     return(
-        <h1> Welcome to petshop </h1>
+        <h1 style={style}> 
+        Welcome to petshop 
+        </h1>
     );
 };
 
 
 module.exports = HomePage;
+
