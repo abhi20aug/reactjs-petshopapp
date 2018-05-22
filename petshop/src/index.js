6f642f207e024158ec955671c3a702a7e668ac73
Abhishek Agrawal <abhi20aug@gmail.com>
2018-05-22 22:17:34 +0530

    add index.js

--- petshop/src/index.js
+++ petshop/src/index.js
@@ -1,8 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//display on UI
