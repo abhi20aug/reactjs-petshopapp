842a6c0a5019f951e57d4844157af0e09fac417b
Abhishek Agrawal <abhi20aug@gmail.com>
2018-05-22 23:38:51 +0530

    img close tag

--- petshop/src/components/CatComponent.js
+++ petshop/src/components/CatComponent.js
@@ -11,7 +11,7 @@ class CatComponent extends React.Component{
  return (
     <div>
         <h3> Cat Component </h3>
-        <img style= {style} src="https://i.ytimg.com/vi/vqc8oezvs8I/maxresdefault.jpg" alt="Cute Cat">      
+        <img style= {style} src="https://i.ytimg.com/vi/vqc8oezvs8I/maxresdefault.jpg" alt="Cute Cat"/>      
     </div>
  );  
  };
