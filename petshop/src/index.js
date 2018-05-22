0a39351317703e4d6860a3d94b9986090ed5a589
Abhishek Agrawal <abhi20aug@gmail.com>
2018-05-22 22:20:54 +0530

    index.js-1

--- petshop/src/index.js
+++ petshop/src/index.js
@@ -2,3 +2,16 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 //display on UI
+//home page component
+//function that return jsx
+
+var HomePage = function(){
+    return(
+        <h1> Welcome to petshop </h1>
+    );
+};
+
+ReactDOM.render(
+    <HomePage/>,
+    document.getElementById('root')
+);
